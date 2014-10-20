cd48d1f9b0555019f3a9d0ad26784c877fc69e45
Yaser Jarrar <yaser@grout.io>
2014-10-20 13:21:21 +0400

    Bump version to 0.1.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Standard Mylar packages",
     name: "mylar:platform",
-    version: "0.0.1",
+    version: "0.1.0",
     git: "https://github.com/gliesesoftware/mylar.git"
 });
 
